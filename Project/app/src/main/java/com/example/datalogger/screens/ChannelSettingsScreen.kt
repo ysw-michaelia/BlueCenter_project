2099e290b9607feeff6447811cdfeeca7bfc9cbd
Shuwen Yang <yangshuwen7560@gmail.com>
2024-11-02 18:57:16 +0100

    dropdown menu updated
    you can only choose one sensor each time before resetting

--- Project/app/src/main/java/com/example/datalogger/screens/ChannelSettingsScreen.kt
+++ Project/app/src/main/java/com/example/datalogger/screens/ChannelSettingsScreen.kt
@@ -121,10 +121,12 @@ fun ChannelSettingsScreen(
                 else{
                     Text("Sensor: ${channel.sensorName}")
                     //dropdown menu to choose sensor
-                    SensorDropdownMenu(onSensorSelected = { selectedSensor ->
-                        editedSensorName = selectedSensor.name
-                        editedSensorType = selectedSensor.type
-                    })
+                    if(channel.sensorType == 0) {
+                        SensorDropdownMenu(onSensorSelected = { selectedSensor ->
+                            editedSensorName = selectedSensor.name
+                            editedSensorType = selectedSensor.type
+                        })
+                    }
                 }
 
             }
